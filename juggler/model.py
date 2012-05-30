e4bacc9f5004f1600e652f3529293e155f0d90bb
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-05-30 18:09:44 +0200

    add more model bits

--- juggler/model.py
+++ juggler/model.py
@@ -1,10 +1,50 @@
 
-from couchdbkit.schema import Document
+from couchdbkit.schema import Document #, DocumentSchemaProperty
+from couchdbkit.schema import DateTimeProperty, StringProperty
 from reprtools import FormatRepr
-
+from datetime import datetime
 
 class Driver(Document):
     doc_type = 'juggler:driver'
 
     current_host = str
     current_pid = int
+    last_use = datetime
+
+    #: one of one of cron, shedule, inbox or make
+    intent = str
+
+
+
+class Project(Document):
+    doc_type = 'juggler:project'
+
+
+class Task(Document):
+    #XXX: dummy
+    __rerp__ = FormatRepr('<Task {belongs_to} {status}>')
+
+class Step(Document):
+    doc_type = 'juggler:step'
+
+    __repr__ = FormatRepr('<Step of {task} started {started:%Y-%m-%d}>')
+
+    task = str
+    status = StringProperty(default='prepared')
+    inputs = dict
+    steper = str
+    started = DateTimeProperty(default=datetime.utcnow)
+    finished = DateTimeProperty(default=None)
+
+class Event(Document):
+    doc_type = 'juggler:event'
+    __repr__ = FormatRepr(r'<Event {step} {index}>')
+
+    step = str
+    index = int
+
+
+
+
+
+
