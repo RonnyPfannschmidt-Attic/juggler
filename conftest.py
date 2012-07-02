cfd1fc43c8bd02017eaa665fe02deb774a2089ef
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-07-02 18:07:44 +0200

    switch from kanso to couchdb-compose

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 import py
 root = py.path.local(__file__).dirpath()
-kansoapp = root/'kansoapp'
+composeapp = root/'composeapp'
 
 def pytest_funcarg__ghost_base(request):
     db = request.getfuncargvalue('couchdb')
     return db.res.uri + '/_design/juggler/_rewrite'
 
 def pytest_couchdbkit_push_app(dbname):
-    py.std.subprocess.check_call(['kanso', 'install'], cwd=str(kansoapp))
-    py.std.subprocess.check_call(['kanso', 'push', dbname], cwd=str(kansoapp))
+    py.std.subprocess.check_call([
+        'couchdb-compose', 'push', dbname,
+                           '--path', str(composeapp)
+    ])
 
 def pytest_funcarg__juggler(request):
     db = request.getfuncargvalue('couchdb')
