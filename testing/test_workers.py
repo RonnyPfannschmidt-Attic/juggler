3f4b8ee3428f8e137c253af85ba523818396db73
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-07-10 17:01:54 +0200

    more tests

--- testing/test_workers.py
+++ testing/test_workers.py
@@ -2,6 +2,7 @@ import pytest
 from mock import Mock
 from juggler import workers
 from juggler import model
+from couchdbkit.exceptions import ResourceConflict
 
 
 def faked_watch_for(result, info=None):
@@ -39,7 +40,7 @@ def test_valid_order_simple_ready():
         {'test': 'a'},
         {'test':'b'}
     ]),
-])
+], ids=['nospec', 'somespec'])
 def test_ready_order_generate_tasks(axis, specs):
     db = Mock()
     order = model.Order(status='ready', axis=axis)
@@ -53,3 +54,43 @@ def test_ready_order_generate_tasks(axis, specs):
     for task, spec in zip(items, specs):
 
         assert task.spec == spec
+
+
+def test_new_task_generate_steps_programmatic_unimplemented():
+    db = Mock()
+    project = model.Project(computed_steps=True)
+    db.get.return_value = project
+    task = model.Task(status='new', project='blabla')
+    watch_for = faked_watch_for(task)
+    pytest.raises(
+        NotImplementedError,
+        workers.new_task_generate_steps,
+        db, watch_for)
+
+
+def test_new_task_generate_from_template():
+    db = Mock()
+    project = model.Project()
+    db.get.return_value = project
+    task = model.Task(status='new', project='blabla')
+    watch_for = faked_watch_for(task)
+    workers.new_task_generate_steps(db, watch_for)
+
+    items = db.bulk_save.call_args[0][0]
+    saved_task = items.pop(0)
+    assert saved_task is task
+    assert task.status == 'pending'
+    #XXX: check items
+
+
+@pytest.mark.parametrize('conflict', [False, True],
+                         ids=['pass', 'conflict'])
+def test_claim_pending_task(conflict):
+    db = Mock()
+    task = model.Task(status='pending', project='blabla')
+    watch_for = faked_watch_for(task)
+
+    if conflict:
+        db.save_doc.side_effect = ResourceConflict()
+    workers.claim_pending_task(db, watch_for, 'test')
+    db.save_doc.assert_called_with(task)
