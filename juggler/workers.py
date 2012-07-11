355fecb1804e27dc0f0b386086332ba995cc7f24
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-07-11 23:24:29 +0200

    create a watches_for decorator

--- juggler/workers.py
+++ juggler/workers.py
@@ -1,5 +1,6 @@
 import copy
 from itertools import product
+from functools import wraps
 from couchdbkit.exceptions import ResourceConflict
 from .model import Order, Task, Project
 
@@ -18,22 +19,39 @@ def generate_specs(axis):
         yield dict(zip(names, values))
 
 
-def inbox_validate(db):
-    order, _ = db.watch_for(Order, status='received')
+def watches_for(type, status, **wkw):
+    def decorator(func):
+        @wraps(func)
+        def watching_version(db, *k, **kw):
+            watch_kw = {}
+            for key, val in wkw.items():
+                watch_kw[key] = val(kw)
+
+            item, _ = db.watch_for(type, status=status, **watch_kw)
+            return func(db, item, *k, **kw)
+        watching_version.type = type
+        watching_version.status = status
+        watching_version.func = func
+        return watching_version
+    return decorator
+
+
+@watches_for(Order, 'received')
+def inbox_validate(db, order):
     #XXX: validate
     order.status = 'valid'
     db.save_doc(order)
 
 
-def valid_order_prepare(db):
-    order, _ = db.watch_for(Order, status='valid')
+@watches_for(Order, 'valid')
+def valid_order_prepare(db, order):
     #XXX: fill in axis from project/task
     order.status = 'ready'
     db.save_doc(order)
 
 
-def ready_order_generate_tasks(db):
-    order, _ = db.watch_for(Order, status='ready')
+@watches_for(Order, 'ready')
+def ready_order_generate_tasks(db, order):
     bulk = [order]
     oid = order._id
     for idx, spec in enumerate(generate_specs(order.axis)):
@@ -52,8 +70,8 @@ def ready_order_generate_tasks(db):
     db.bulk_save(bulk)
 
 
-def new_task_generate_steps(db):
-    task, _ = db.watch_for(Task, status='new')
+@watches_for(Task, 'new')
+def new_task_generate_steps(db, task):
     project = db.get(task.project, schema=Project)
     bulk = [task]
     if project.computed_steps:
@@ -65,8 +83,8 @@ def new_task_generate_steps(db):
     db.bulk_save(bulk)
 
 
-def claim_pending_task(db, owner):
-    task, _ = db.watch_for(Task, status='pending')
+@watches_for(Task, 'pending')
+def claim_pending_task(db, task, owner):
     task.status = 'claiming'
     task.owner = owner
     try:
@@ -80,10 +98,10 @@ def claim_pending_task(db, owner):
         pass
 
 
-def approve_claimed_task(db):
+@watches_for(Task, 'claiming')
+def approve_claimed_task(db, task):
     # this asumes only one claim manager is running ever
     # we operate on a first come first serve basis
-    task, info = db.watch_for(Task, status='claiming')
     all_docs = db.all_current_docs_for(task)
 
     for doc in all_docs:
@@ -99,6 +117,6 @@ def approve_claimed_task(db):
         db.save_doc(task)
 
 
-def run_one_claimed_task(db, owner):
-    task, _ = db.watch_for(Task, status='claimed', owner=owner.name)
+@watches_for(Task, 'claimed', owner=lambda kw: kw['owner'].name)
+def run_one_claimed_task(db, task, owner):
     owner.run(task)
