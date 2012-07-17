88527e1a2f2db8541782a57e0e0eed0e215e4cc5
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-07-18 01:27:00 +0200

    actually runnable master script

--- bin/master.py
+++ bin/master.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
-
+import sys
+from juggler.service import Juggler
+from juggler.handlers.utils import get_database
 from juggler import simple_master
-simple_master()
+db = get_database(sys.argv[1])
+juggler = Juggler(db, 'master')
+simple_master.simple_master(juggler)
