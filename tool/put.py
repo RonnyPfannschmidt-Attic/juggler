2b07826bca7f4d1bfb0774c52bc669a0c3e87679
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-07-18 11:21:29 +0200

    fix misstakes in put tool

--- tool/put.py
+++ tool/put.py
@@ -1,22 +1,24 @@
 import argparse
 import yaml
-
+from juggler.handlers.utils import get_database
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('database')
 parser.add_argument('input')
-parser.add_argument('--newid', nargs=1)
-
+parser.add_argument('--newid')
 
 args = parser.parse_args()
 
-from juggler.handlers.utils import get_database
+def main(db, args):
+    with open(args.input) as fp:
+        data = yaml.load(fp)
 
-db = get_database(args.database)
-with open(args.input) as fp:
-    data = yaml.load(fp)
-if args.newid:
-    data['_id'] = parser.newid
+    if args.newid:
+        data['_id'] = args.newid
 
-db.save_doc(data)
+    db.save_doc(data)
+
+if __name__ == '__main__':
+    db = get_database(args.database)
+    main(db, args)
