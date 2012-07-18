a77c1dee26ca662667c2c84d3a26382007f38c2e
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-07-18 12:50:28 +0200

    fix restcli call

--- tool/example-reset-db.sh
+++ tool/example-reset-db.sh
@@ -1,4 +1,4 @@
-restcli http://localhost:5984/juggler DELETE
+restcli http://localhost:5984/juggler -X DELETE
 couchdb-compose push juggler --path composeapp
 echo put project
 python tool/put.py juggler tool/example_project.yml
