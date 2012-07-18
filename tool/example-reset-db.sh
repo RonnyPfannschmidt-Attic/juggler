50bf44be2707f4b20156427551cf740cdd28fe4d
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-07-18 12:56:27 +0200

    some final fixes the simple test works now

--- tool/example-reset-db.sh
+++ tool/example-reset-db.sh
@@ -1,4 +1,6 @@
+echo DELETE db juggler
 restcli http://localhost:5984/juggler -X DELETE
+echo couchdb-compose
 couchdb-compose push juggler --path composeapp
 echo put project
 python tool/put.py juggler tool/example_project.yml
