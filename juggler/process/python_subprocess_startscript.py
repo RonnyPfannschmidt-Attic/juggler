757391acc114ab8727c50c950ffdb1b36387c042
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-08-02 23:04:00 +0200

    add the beginning of the python subprocess start script

---
+++ juggler/process/python_subprocess_startscript.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+class FlushFile(object):
+    """Write-only flushing wrapper for file-type objects."""
+    def __init__(self, f):
+        self.f = f
+    def write(self, x):
+        self.f.write(x)
+        self.f.flush()
+
+# Replace stdout with an automatically flushing version
+sys.stdout = FlushFile(sys.__stdout__)
+
+script = sys.stdin.read()
+
+code = compile(script, '<script>')
+
+exec(code)
