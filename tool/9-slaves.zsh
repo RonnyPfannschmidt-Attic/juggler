2070dacb8f28c5604f17caedcc9cbf5a692d6605
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2012-09-12 20:16:17 +0200

    changes to the 9-slaves script

--- tool/9-slaves.zsh
+++ tool/9-slaves.zsh
@@ -1,5 +1,8 @@
 for number in {0..9}
 do
-    python bin/slave.py juggler slave$number simple&
+    python bin/slave.py juggler slave${number} simple&
 done
-wait %?slave || kill %?slave
+wait %?slave || while kill %?slave
+do
+    true;
+done
